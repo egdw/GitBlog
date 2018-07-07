bd68b3c968557b8eb67b8b9429a56131250c334b
hdy <378759617@qq.com>
2018-07-07 21:31:16 +0800

    日常备份

--- js/main.js
+++ js/main.js
@@ -1,10 +1,51 @@
+// 当前项目的仓库地址
+var currentRepositoryUrl;
+// 当前项目的配置地址
+var currentConfigUrl;
+// 当前文章的地址
+var currentPageUrl;
+// 当前api的地址
+var currentApiUrl;
+
+
+
+Vue.component("nopicli", {
+    template: '<li class="flow-list-li"><a><div class="summary"><h3 class="title">{{pagetitle}}</h3><p class="abstract">{{desc}}</p></div></a></li>',
+    props: ['item'],
+    data: function () {
+        model: this.item
+    },
+    computed: {
+        pagetitle: function () {
+            console.log(this.model.name)
+            return this.model.name
+        },
+        desc: function () {
+
+        },
+        pic: function () {
+
+        }
+    },
+
+})
+
+Vue.component("nopicli", {
+    template: '<li class="flow-list-li"><img class="wrap-img" src=""><a><div class="summary"><h3 class="title">{{title}}</h3><p class="abstract">{{desc}}</p></div></a></li>',
+    props: ['title', 'desc', 'pic'],
+    data: function () {
+
+    }
+})
+
+
 var vm = new Vue({
     el: '#main',
     methods: {
 
     },
     data: {
-
+        pages: []
     }
 })
 
@@ -24,12 +65,7 @@ function compileMarkDown() {
 $(document).ready(function () {
 
     // 获取配置文件的地址
-    host = window.location.host;
-    host2 = document.domain;
     url = window.location.href;
-
-    console.log(host)
-    console.log(host2)
     url = url.replace("/index.html", "/")
     if (url.length > 0) {
         if (url.charAt(url.length - 1) == '/') {
@@ -39,14 +75,35 @@ $(document).ready(function () {
             url = url + "/assets/config.json";
         }
     }
+    //获取到当前配置文件的路径
+    this.currentConfigUrl = url;
     console.log(url)
-    // $.ajax(2{
-    //     type: "get",
-    //     url: "url",
-    //     data: "data",
-    //     dataType: "dataType",
-    //     success: function (response) {
-
-    //     }
-    // });
+    $.ajax({
+        type: "get",
+        url: url,
+        dataType: "json",
+        success: function (data, response) {
+            //获取到当前项目的url地址
+            this.currentRepositoryUrl = data.repository_url
+            this.currentApiUrl = data.api_url
+            //获取当前项目的文章地址
+            this.currentPageUrl = this.currentApiUrl + "/contents/pages?ref=master"
+            //进行ajax请求.获取相应的数据
+            console.log(this.currentRepositoryUrl)
+            console.log(this.currentPageUrl)
+            //获取文章..
+            $.ajax({
+                type: "get",
+                url: this.currentPageUrl,
+                dataType: "json",
+                success: function (data, response) {
+                    console.log(data)
+                    vm.pages = data;
+                    // for (var i = 0; i < data.length; i++) {
+                    //     console.log(data[i])
+                    // }
+                }
+            });
+        }
+    });
 });
