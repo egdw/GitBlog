27227ca44193c974782659d86f1c4cdcd0541a6a
hdy <378759617@qq.com>
2018-07-07 19:45:51 +0800

    测试配置文件是否能够读取

--- js/main.js
+++ js/main.js
@@ -1,20 +1,52 @@
 var vm = new Vue({
-    el: '#test',
+    el: '#main',
     methods: {
-        testfun: function () {
-            alert("事件触发了")
-            $.ajax({
-                type: "get",
-                url: "https://api.github.com/repos/egdw/temp_pic_upload",
-                dataType: "json",
-                success: function (data, response) {
-                    console.log(response)
-                    console.log(data)
-                }
-            });
-        }
+
     },
     data: {
-        test: 'test'
+
     }
 })
+
+
+// 把markdown转换为html代码.通过使用showdown.js
+function compileMarkDown() {
+    //获取要转换的文字
+    var text = document.getElementById("content").value;
+    //创建实例
+    var converter = new showdown.Converter();
+    //进行转换
+    var html = converter.makeHtml(text);
+    //展示到对应的地方
+
+}
+
+$(document).ready(function () {
+
+    // 获取配置文件的地址
+    host = window.location.host;
+    host2 = document.domain;
+    url = window.location.href;
+
+    console.log(host)
+    console.log(host2)
+    url = url.replace("/index.html", "/")
+    if (url.length > 0) {
+        if (url.charAt(url.length - 1) == '/') {
+            // 如果后面有斜杠的话
+            url = url + "assets/config.json";
+        } else {
+            url = url + "/assets/config.json";
+        }
+    }
+    console.log(url)
+    // $.ajax(2{
+    //     type: "get",
+    //     url: "url",
+    //     data: "data",
+    //     dataType: "dataType",
+    //     success: function (response) {
+
+    //     }
+    // });
+});
